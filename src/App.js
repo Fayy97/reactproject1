3fade859ca86fe9272c6ed06be64e369569c8091
Rashford <mikearowo@gmail.com>
2023-03-06 00:01:48 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import "./App.css";
+import { Header } from "./Header";
+import { Content } from "./Content";
+import { Footer } from "./Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/Employees.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+
+export const Employees = () => {
+
+    const [employees, setEmployees] = useState([])
+  return <main></main>;
+};
